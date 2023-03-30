464cc9496e192d189760296a65e42e568a481883
tgoodall3 <Tylergoodall3@gmail.com>
2023-03-30 12:13:53 -0400

    Updated Code

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,6 @@
 var usersExist = false;
 var userFullName = "";
+var _db = "";
 
 function barlistener(){
     $(".bars").click(function(e){
@@ -37,13 +38,19 @@ function initURLlisteners(){
 function initFirebase(){
   firebase.auth().onAuthStateChanged((user)=> {
     if(user){
+      _db = firebase.firestore();
       console.log("auth changed logged in");
 
+      console.log("display name");
+      console.log(user.displayName);
+      
       if (user.displayName){
         $(".name").html(user.displayName);
       }
+      
       usersExist = true;
     } else{
+      _db = "";
       console.log("auth changed logged out");
       $(".name").html("");
 
@@ -87,11 +94,19 @@ function signIn() {
 function createAccount() {
   let FName = $("#FName").val();
   let LName = $("#LName").val();
-  let Email = $("#Email").val();
-  let Password = $("#Password").val();
+  let Email = $(".Email").val();
+  let Password = $(".Password").val();
   let fullname = FName + ' ' + LName;
+  let userObj = {
+    firstname: FName,
+    lastname: LName,
+    Email: Email,
+    lists: []
+  }
+
 
   console.log("create " + FName + ' ' + LName + ' ' + Email + ' ' + Password + ' ');
+  console.log("email" + Email);
 
   firebase.auth().createUserWithEmailAndPassword(Email, Password)
   .then((userCredential) => {
@@ -100,11 +115,25 @@ function createAccount() {
     // ...
 
     console.log("created");
-    alert ("You have created your account!");
+
+    alert("You have created your account!");
 
     firebase.auth().currentUser.updateProfile({
-      displayName: fullname
+      displayName: fullname,
     })
+
+   _db.collection("Users").doc(user.uid).set(userObj).then((doc) => {
+    console.log("doc added");
+   })
+   .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+    console.log("create error " + errorMessage);
+
+    console.log(displayName);
+  });
+
     userFullName = fullname;
   })
   .catch((error) => {
